7c65aaab2a81ffdce5cc2651b275c827aa427b48
spurs0929 <briankang0929@gmail.com>
2023-01-08 21:53:46 +0800

    feat: 新增BFS, DFSPreOrder, DFSInOrder, DFSPostOrder

--- Tree/BinarySearchTree/BinarySearchTree.js
+++ Tree/BinarySearchTree/BinarySearchTree.js
@@ -102,4 +102,66 @@ class BinarySearchTree{
     // 節點不存在根節點中回傳false
     return false;
   }
+  // 深度搜尋
+  BFS(){
+    // node初始為根節點
+    var node = this.root;
+    // result
+    var data = [];
+    // temp
+    var queue = [];
+    queue.push(node);
+
+    // queue陣列有值
+    while(queue.length){
+      node = queue.shift();
+      // node.value值存入結果陣列
+      data.push(node.value);
+      // 判斷左節點存在
+      if(node.left) queue.push(node.left);
+      // 判斷右節點存在
+      if(node.right) queue.push(node.right);
+    }
+    // 回傳結果
+    return data;
+  }
+  // 廣度搜尋(前序)
+  DFSPreOrder(){
+    var data = [];
+    // tree traverse
+    function traverse(node){
+      // 根節點 -> 左節點 -> 右節點
+      data.push(node.value);
+      if(node.left) traverse(node.left);
+      if(node.right) traverse(node.right);
+    }
+    traverse(this.root);
+    return data;
+  }
+  // 廣度搜尋(中序)
+  DFSInOrder(){
+    var data = [];
+    // tree traverse
+    function traverse(node){
+      // 左節點 -> 根節點 -> 右節點
+      if(node.left) traverse(node.left);
+      data.push(node.value);
+      if(node.right) traverse(node.right);
+    }
+    traverse(this.root);
+    return data;
+  }
+  // 廣度搜尋(後序)
+  DFSPostOrder(){
+    var data = [];
+    // tree traverse
+    function traverse(node){
+      // 左節點 -> 右節點 -> 根節點
+      if(node.left) traverse(node.left);
+      if(node.right) traverse(node.right);
+      data.push(node.value);
+    }
+    traverse(this.root);
+    return data;
+  }
 }
